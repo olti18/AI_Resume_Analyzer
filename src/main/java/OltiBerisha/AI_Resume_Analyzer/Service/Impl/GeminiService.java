2ad362ae1db2d1fe281b736eb0ffa0849d3ae8c4
Olti Berisha <olti.berisha.150@gmail.com>
2025-04-20 22:16:58 +0200

    Create GeminiService.java

---
+++ src/main/java/OltiBerisha/AI_Resume_Analyzer/Service/Impl/GeminiService.java
@@ -0,0 +1,14 @@
+package OltiBerisha.AI_Resume_Analyzer.Service.Impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GeminiService {
+
+    private static final String API_KEY = "//";
+    private static final String GEMINI_ENDPOINT = "https://api.gemini.com/v1/public/";
+
+
+
+
+}
