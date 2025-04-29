78d777567112c59f6d561b89de8e5c00059d5573
Olti Berisha <olti.berisha.150@gmail.com>
2025-04-29 22:41:13 +0200

    Create KeycloakUtils.java

---
+++ src/main/java/OltiBerisha/AI_Resume_Analyzer/Config/KeycloakUtils.java
@@ -0,0 +1,16 @@
+package OltiBerisha.AI_Resume_Analyzer.Config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+public class KeycloakUtils {
+    private  String getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof Jwt) {
+            Jwt jwt = (Jwt) authentication.getPrincipal();
+            return jwt.getClaim("sub"); // ID-ja e përdoruesit nga Keycloak
+        }
+        return null;
+    }
+}
